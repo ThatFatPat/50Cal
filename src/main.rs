80455f53fdbc46a03fbe48b5a4ea4e5c5c7eb236
ThatFatPat <idoshav@gmail.com>
2020-09-26 17:05:04 +0300

    Further broken down code

--- src/main.rs
+++ src/main.rs
@@ -10,25 +10,30 @@ fn main() -> Result<(), Box<dyn Error>> {
         io::stdout().flush()?;
         let mut input = String::new();
         io::stdin().read_line(&mut input)?;
-        let mut cmdline = input.split_whitespace();
-        let command_name = match cmdline.next() {
-            Some(x) => x,
-            None => continue,
-        };
-        if command_name == "exit" {
-            return Ok(());
-        }
-        let mut command = process::Command::new(&command_name);
-        match spawn(command.args(cmdline)) {
-            Ok(_status) => {}
-            Err(x) => eprintln!(
-                "Failed to execute command: `{}` with error: {}",
-                &command_name, x
-            ),
-        }
+        process_cmdline(&input)?;
     }
 }
-
+fn process_cmdline(line: &str) -> Result<(), Box<dyn Error>> {
+    let mut cmdline = line.split_whitespace();
+    let command_name = match cmdline.next() {
+        Some(x) => x,
+        None => return Ok(()),
+    };
+    match command_name {
+        "exit" => process::exit(0),
+        _ => {
+            let mut command = process::Command::new(&command_name);
+            match spawn(command.args(cmdline)) {
+                Ok(_status) => {}
+                Err(x) => eprintln!(
+                    "Failed to execute command: `{}` with error: {}",
+                    &command_name, x
+                ),
+            };
+        }
+    }
+    Ok(())
+}
 fn spawn(command: &mut process::Command) -> Result<process::ExitStatus, Box<dyn Error>> {
     Ok(command.spawn()?.wait()?)
 }
